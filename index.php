ef37a7751f4a29518d4613649f5ecce6d71f9d7b
Dhiraj <dhirajshrivastav@outlook.com>
2013-04-20 12:42:44 +0530

    first commit on SbRev01

--- index.php
+++ index.php
@@ -3,14 +3,6 @@
 
 // Include the necessary files
 
-// include_once 'inc/functions.inc.php';
-// include_once 'inc/db.inc.php';
-// include_once ('inc/functions.inc.php');
-// include_once ('inc/db.inc.php');
-//The above method of including files was not working so placed them under single line comments
-//Just for testing adding a comment for checking
-// Changes for first commit.
-//Changes for the second commit.
 include_once 'inc/function.inc.php';
 include_once 'inc/db.inc.php';
 
