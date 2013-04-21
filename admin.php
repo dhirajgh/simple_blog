4a8033ff10441da9bd42ca43e80553d5503ce23a
Dhiraj <dhirajshrivastav@outlook.com>
2013-04-21 15:47:49 +0530

    Commit till fig 7.2 Edit and update entries - delete

--- admin.php
+++ admin.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+* Include the necessary files
+*/
+include_once 'inc/function.inc.php';
+include_once 'inc/db.inc.php';
+// Open a database connection
+$db = new PDO(DB_INFO, DB_USER, DB_PASS);
+
+
 if(isset($_GET['page']))
 {
 $page = htmlentities(strip_tags($_GET['page']));
@@ -7,6 +17,30 @@ else
 {
 $page = 'blog';
 }
+
+        if(isset($_GET['url']))
+        {
+            // Do basic sanitization of the url variable
+            $url = htmlentities(strip_tags($_GET['url']));
+            // Set the legend of the form
+            $legend = "Edit This Entry";
+            // Load the entry to be edited
+            $e = retrieveEntries($db, $page, $url);
+            // Save each entry field as individual variables
+            $id = $e['id'];
+            $title = $e['title'];
+            $entry = $e['entry'];
+        }
+        else
+        {
+                // Set the legend
+                $legend = "New Entry Submission";
+                // Set variables to NULL if not editing
+                $id = NULL;
+                $title = NULL;
+                $entry = NULL;
+        }
+
 ?>
 <!DOCTYPE html
 PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -26,10 +60,19 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	
 		<form method="post" action="/simple_blog/inc/update.inc.php">
 			<fieldset>
-				<legend>New Entry Submission</legend>  
-				<label>Title<input type="text" name="title" maxlength="150" /></label>
-				<label>Entry<textarea name="entry" cols="45" rows="10"></textarea></label>
-                <input type="hidden" name="page" value="<?php echo $page ?>" />
+				<legend><?php echo $legend ?></legend> 
+				<label>Title
+                <input type="text" name="title" maxlength="150"
+                value="<?php echo htmlentities($title) ?>" />
+                </label>
+				<label>Entry
+                <textarea name="entry" cols="45"
+                rows="10"><?php echo sanitizeData($entry) ?></textarea>
+                </label>
+               <input type="hidden" name="id"
+                value="<?php echo $id ?>" />
+                <input type="hidden" name="page"
+                value="<?php echo $page ?>" />
 				<input type="submit" name="submit" value="Save Entry" />
 				<input type="submit" name="submit" value="Cancel" />
 			</fieldset>
