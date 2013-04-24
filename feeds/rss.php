5a4f37665d637a33a2eedf0ed42930acea1abe8d
Dhiraj <dhirajshrivastav@outlook.com>
2013-04-24 20:13:19 +0530

    making new branch for rss

---
+++ feeds/rss.php
@@ -0,0 +1,23 @@
+<?php
+
+// Add a content type header to ensure proper execution
+header('Content-Type: application/rss+xml');
+
+// Output the XML declaration
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+//This does not work in Chrome but works in Internet Explorer
+//Works perfectly well in Mozilla Browser
+
+?>
+
+
+
+<rss version="2.0">
+<channel>
+<title>My Simple Blog</title>
+<link>http://localhost/simple_blog/</link>
+<description>This blog is awesome.</description>
+<language>en-us</language>
+</channel>
+</rss>
