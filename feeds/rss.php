cc13c69224c8a40bba96fb0c26e8a5851736f0e5
Dhiraj <dhirajshrivastav@outlook.com>
2013-04-28 09:56:00 +0530

    Before starting the analysis

--- feeds/rss.php
+++ feeds/rss.php
@@ -1,4 +1,21 @@
-<?php
+ <?php
+
+// Include necessary files
+include_once '../inc/function.inc.php';
+include_once '../inc/db.inc.php';
+
+
+// Open a database connection
+$db = new PDO(DB_INFO, DB_USER, DB_PASS);
+
+// Load all blog entries
+$e = retrieveEntries($db, 'blog');
+
+// Remove the fulldisp flag
+array_pop($e);
+
+// Perform basic data sanitization
+$e = sanitizeData($e);
 
 // Add a content type header to ensure proper execution
 header('Content-Type: application/rss+xml');
@@ -18,5 +35,24 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 		<link>http://localhost/simple_blog/</link>
 		<description>This blog is awesome.</description>
 		<language>en-us</language>
+		
+		<?php
+		// Loop through the entries and generate RSS items
+		foreach($e as $e):
+		
+		// Escape HTML to avoid errors
+		$entry = htmlentities($e['entry']);
+		
+		// Build the full URL to the entry
+		$url = 'http://localhost/simple_blog/blog/' . $e['url'];
+		?>
+		
+		<item>
+		<title><?php echo $e['title']; ?></title>
+		<description><?php echo $entry; ?></description>
+		<link><?php echo $url; ?></link>
+		</item>
+		<?php endforeach; ?>	
+			
 </channel>
 </rss>
